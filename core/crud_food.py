e8eda6cdd5bfa9d00b2eb85fb1c939c2bc1bd443
Christoffer Jansson <christofferjansson1@gmail.com>
2024-08-01 22:35:37 +0200

    delete food

--- core/crud_food.py
+++ core/crud_food.py
@@ -1,7 +1,9 @@
 import pyodbc
 import pandas as pd
 import streamlit as st
+
 from core.connection import connect_to_app_database
+from core.logging import log_action
 
 def get_all_food_data(conn, table="food"):
     try:
@@ -36,3 +38,21 @@ def insert_food_production(conn, food_name, production_date, quantity, goal_reac
         st.success("Record inserted successfully.")
     except pyodbc.Error as e:
         st.error(f"Error inserting record into 'food_production' table: {e}")
+
+def delete_food_data(conn, username, production_id):
+    try:
+        cursor = conn.cursor()
+
+        # Check if the food item exists
+        cursor.execute("SELECT COUNT(*) FROM dbo.food_production WHERE production_id = ?", (production_id,))
+        if cursor.fetchone()[0] == 0:
+            st.error(f"Food item with ID {production_id} does not exist.")
+            return
+               
+        # Delete the food item
+        cursor.execute("DELETE FROM dbo.food_production WHERE production_id = ?", (production_id,))
+        conn.commit()
+        st.success(f"Deleted food item with ID {production_id} from 'food_production' table")
+        log_action(username, production_id, f"Deleted food item with ID {production_id}")
+    except pyodbc.Error as e:
+        st.error(f"Error deleting data: {e}")
